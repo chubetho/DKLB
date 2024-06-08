a353f6566ada9723450bc683be4f0171079cf747
phatnguyen <phatnguyen@gmx.de>
2024-06-08 15:31:05 +0200

    refactore: routes

--- apps/shell/src/router.ts
+++ apps/shell/src/router.ts
@@ -1,7 +1,7 @@
 import type { App } from 'vue'
-import { type RouteRecordRaw, createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import mfeConfig from '@dklb/mfe-config'
-import { useRemote } from '../src/utils/mfe'
+import { loadRoutes } from '../src/utils/mfe'
 
 export async function setupRouter(app: App) {
   const router = createRouter({
@@ -23,34 +23,4 @@ export async function setupRouter(app: App) {
   routes.forEach(route => router.addRoute(route))
 
   app.use(router)
-}
-
-async function loadRoutes(entries: { name: string, prefix: string }[]) {
-  const promises = entries.map(async ({ name, prefix }) => {
-    try {
-      const { getModule } = await useRemote(name)
-      const routes = await getModule<{ path: string, component: string }[] | undefined>('routes')
-      if (!routes || !routes.length)
-        return []
-
-      return routes.map(route => ({
-        path: prefix === '/' ? route.path : `${prefix}${route.path}`,
-        component: async () => getModule(route.component),
-      }))
-    }
-    catch (e) {
-      console.error(`Error loading routes from ${name}: `, e)
-    }
-  })
-
-  const routes: RouteRecordRaw[] = []
-  const results = await Promise.allSettled(promises)
-  results.forEach((result) => {
-    if (result.status === 'rejected' || !result.value)
-      return
-
-    routes.push(...result.value)
-  })
-
-  return routes
 }
--- apps/shell/src/utils/mfe.ts
+++ apps/shell/src/utils/mfe.ts
@@ -1,10 +1,41 @@
 import { __federation_method_getRemote, __federation_method_unwrapDefault } from 'virtual:__federation__'
+import type { RouteRecordRaw } from 'vue-router'
 
-export async function useRemote(name: string) {
+async function useRemote(name: string) {
   const getModule = async <T>(path: string) => {
     const wrapped = await __federation_method_getRemote(name, `./${path}`)
     return __federation_method_unwrapDefault(wrapped) as T
   }
 
   return { getModule }
+}
+
+export async function loadRoutes(entries: { name: string, prefix: string }[]) {
+  const promises = entries.map(async ({ name, prefix }) => {
+    try {
+      const { getModule } = await useRemote(name)
+      const routes = await getModule<{ path: string, component: string }[] | undefined>('routes')
+      if (!routes || !routes.length)
+        return []
+
+      return routes.map(route => ({
+        path: prefix === '/' ? route.path : `${prefix}${route.path}`,
+        component: async () => getModule(route.component),
+      }))
+    }
+    catch (e) {
+      console.error(`Error loading routes from ${name}: `, e)
+    }
+  })
+
+  const routes: RouteRecordRaw[] = []
+  const results = await Promise.allSettled(promises)
+  results.forEach((result) => {
+    if (result.status === 'rejected' || !result.value)
+      return
+
+    routes.push(...result.value)
+  })
+
+  return routes
 }
