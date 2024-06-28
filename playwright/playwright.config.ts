c8785ede023dc6f51e304f43a96c2adccda7fa11
Phat Nguyen <tnguyen@multamedio.de>
2024-06-28 15:01:02 +0200

    ci: fix

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -2,13 +2,14 @@ import process from 'node:process'
 import { defineConfig, devices } from '@playwright/test'
 
 const isCI = !!process.env.CI
+
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!isCI,
   retries: isCI ? 2 : 0,
   workers: isCI ? 1 : undefined,
-  reporter: isCI ? 'html' : 'line',
+  reporter: [isCI ? ['html', { open: 'never' }] : ['list']],
   use: {
     baseURL: 'http://localhost:8000',
     trace: 'on-first-retry',
