7894ad482ed580718daa71c9e4da7f2e028c3e1c
phatnguyen <phatnguyen@gmx.de>
2024-07-14 13:28:44 +0200

    refactor: docker

--- scripts/docker.ts
+++ scripts/docker.ts
@@ -1,24 +1,32 @@
 import { write } from 'bun'
 import mfeConfig from '../tools/mfe-config'
 
-async function dockerfile() {
+async function dockerfileServer() {
+  const content = `FROM oven/bun:slim
+WORKDIR /dklb
+
+COPY ./server ./
+
+RUN sed -i '/"@dklb\/eslint-config": "workspace:\^"/d' ./package.json && \\
+    sed -i '/"@dklb\/tsconfig": "workspace:\^"/d' ./package.json
+
+RUN bun install --production --ignore-scripts
+
+EXPOSE 3000
+ENTRYPOINT ["bun", "run", "start"]
+`
+  await write('dockers/Dockerfile.server', content)
+}
+
+async function dockerfileApps() {
   const cors = `
       add_header 'Access-Control-Allow-Origin' 'http://localhost:8000';
       add_header 'Access-Control-Allow-Methods' 'GET';
       add_header 'Access-Control-Allow-Headers' 'Content-Type';
 `
-  const copies: string[] = []
-  let minPort = Number.POSITIVE_INFINITY
-  let maxPort = Number.NEGATIVE_INFINITY
 
   const entries = Object.values(mfeConfig)
   for (const { port, dir } of entries) {
-    const _port = Number.parseInt(port)
-    if (_port < minPort)
-      minPort = _port
-    if (_port > maxPort)
-      maxPort = _port
-
     const isShell = dir === 'shell'
     const confPath = `.nginx/${dir}.conf`
     const str = `server {
@@ -32,52 +40,59 @@ async function dockerfile() {
       }
 }
 `
-    copies.push(`COPY --from=build /dklb/apps/${dir}/dist ${dir}`)
 
     await write(confPath, str)
-  }
 
-  const content = `FROM oven/bun:slim AS build
+    const content = `FROM oven/bun:slim AS build
 WORKDIR /dklb
 COPY . .
 RUN bun install
 RUN bun run build:ui
-RUN bunx run-p build:apps build:shell
+RUN bun run --cwd apps/${dir} build
 
 FROM nginx:alpine
 WORKDIR /usr/share/nginx/html
 RUN rm -rf * && rm -f /etc/nginx/conf.d/default.conf
 COPY .nginx/*.conf /etc/nginx/conf.d
+COPY --from=build /dklb/apps/${dir}/dist ${dir}
 
-${copies.join('\n')}
-
-EXPOSE ${minPort}-${maxPort}
+EXPOSE ${port}
 ENTRYPOINT ["nginx", "-g", "daemon off;"]
 `
 
-  await write('Dockerfile.apps', content)
-
-  return { minPort, maxPort }
+    await write(`dockers/Dockerfile.${dir}`, content)
+  }
 }
 
-(async function dockercompose() {
-  const { minPort, maxPort } = await dockerfile()
-  const content = `services:
+async function dockercompose() {
+  const entries = Object.values(mfeConfig)
+
+  const contents = [
+    `services:
   server:
     build:
       context: .
-      dockerfile: Dockerfile.server
+      dockerfile: dockers/Dockerfile.server
     ports:
-      - '3000:3000'
+      - '3000:3000'`,
+  ]
 
-  apps:
+  for (const { port, dir } of entries) {
+    const content = `
+  ${dir}:
     build:
       context: .
-      dockerfile: Dockerfile.apps
+      dockerfile: dockers/Dockerfile.${dir}
     ports:
-      - '${minPort}-${maxPort}:${minPort}-${maxPort}'
+      - '${port}:${port}'
     depends_on:
-      - server
-`
-  await write('docker-compose.yml', content)
-})()
+      - server`
+    contents.push(content)
+  }
+
+  await write('docker-compose.yml', contents.join('\n'))
+}
+
+dockerfileServer()
+dockerfileApps()
+dockercompose()
