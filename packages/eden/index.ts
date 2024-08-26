4e6bb963b3fb7a6c04bf92bd58919335ce669def
phatnguyen <phatnguyen@gmx.de>
2024-08-26 20:32:59 +0200

    deploy

--- packages/eden/index.ts
+++ packages/eden/index.ts
@@ -2,6 +2,6 @@ import { treaty } from '@elysiajs/eden'
 import type { App as _App } from '@dklb/api'
 
 export function useApi() {
-  return treaty<_App>('localhost:3000')
+  return treaty<_App>('http://209.38.249.17:3000')
 }
 export type App = _App
